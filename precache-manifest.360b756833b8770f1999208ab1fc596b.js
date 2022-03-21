2df2fd5840f9aa5229edd34d80c1febb992c8bbd
tam11a-rit <ibrahim.tamim@reserveitbd.com>
2022-03-21 16:41:38 +0600

    Updates

---
+++ precache-manifest.360b756833b8770f1999208ab1fc596b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f714c5efa985ddc8ed8bc72406359d59",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bd85a954bd61644d9517",
+    "url": "./static/js/2.723ee99c.chunk.js"
+  },
+  {
+    "revision": "5939b4453a7fd24987154ea96ac5f7fa",
+    "url": "./static/js/2.723ee99c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f671c3445569ed085b18",
+    "url": "./static/js/main.d5e30022.chunk.js"
+  },
+  {
+    "revision": "ae14b0ab1fa48100e50c",
+    "url": "./static/js/runtime-main.20fc085a.js"
+  }
+]);
